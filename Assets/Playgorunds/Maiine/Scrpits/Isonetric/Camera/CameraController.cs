8a1fefd14ec967fff3e7b23f276f68e1e6622dfc
MaiineV <agustinmartinezbarrios2@hotmail.com>
2022-05-15 01:03:12 -0300

    bugfix generales, falta bugfixear el cambio de pjs

--- Assets/Playgorunds/Maiine/Scrpits/Isonetric/Camera/CameraController.cs
+++ Assets/Playgorunds/Maiine/Scrpits/Isonetric/Camera/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour
     Vector3 midCamera;
     Vector3 pointToView;
 
-    bool seeDemon = true; 
+    bool seeDemon = true;
 
     public float speed;
     public float rotationAngle;
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         EventManager.Subscribe("SeeObject", SeeObject);
-        EventManager.Subscribe("ChangeTarget", ChangeTarget);
+        EventManager.Subscribe("ChangePlayer", ChangePlayer);
 
         transform.parent = null;
         GameObject myMain = Camera.main.gameObject;
@@ -39,14 +39,20 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         actualMovement();
+    }
 
-        if (Input.GetMouseButtonDown(1))
+    //Receptor de Botones
+    public void AimButton(bool isDown)
+    {
+        if (isDown)
             SetAim(true);
-        else if (Input.GetMouseButtonUp(1))
+        else if (!isDown)
             SetAim(false);
     }
 
-    public void ChangeTarget(params object[] parameter)
+    #region Cambios del Delegate
+    //Cambia el personaje al que tiene que seguir
+    public void ChangePlayer(params object[] parameter)
     {
         if (seeDemon)
         {
@@ -60,15 +66,16 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    //Activa o desactiva el modo de aim
     public void SetAim(bool isAiming)
     {
-        Debug.Log(actualTransform.gameObject.name);
         if (isAiming)
             actualMovement = Aim;
         else
             actualMovement = FollowPlayer;
     }
 
+    //Mira a un objeto especifico, x un tiempo especifico
     public void SeeObject(params object[] parameter)
     {
         pointToView = (Vector3)parameter[0];
@@ -76,11 +83,16 @@ public class CameraController : MonoBehaviour
         StartCoroutine(SeeObjectoCorroutine((float)parameter[1]));
     }
 
+    #endregion
+
+    #region Posibles Delegate
+    //Sigue al player seleccionado
     void FollowPlayer()
     {
         transform.position += (actualTransform.position - transform.position) * speed * Time.deltaTime;
     }
 
+    //Sigue al punto de aim con limites
     void Aim()
     {
         RaycastHit hit;
@@ -88,29 +100,53 @@ public class CameraController : MonoBehaviour
         {
             Vector3 rayPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             Vector3 dist = (rayPoint - actualTransform.position) * 0.5f;
-            midCamera = rayPoint - dist;
+            midCamera = actualTransform.position + dist;
 
+            float distanceX = actualTransform.position.x - midCamera.x;
+            float distanceZ = actualTransform.position.z - midCamera.z;
 
-            if (midCamera.x > maxDistX)
-                midCamera.x = maxDistX;
-            else if(midCamera.x < -maxDistX)
-                midCamera.x = -maxDistX;
+            distanceX = Mathf.Abs(distanceX);
+            distanceZ = Mathf.Abs(distanceZ);
 
-            if (midCamera.z > maxDistZ)
-                midCamera.z = maxDistZ;
-            else if (midCamera.z < -maxDistZ)
-                midCamera.z = -maxDistZ;
+            Debug.Log(distanceX + " X");
+            Debug.Log(distanceZ + " Z");
+
+            if(distanceX > maxDistX)
+            {
+                if (midCamera.x > actualTransform.position.x)
+                {
+                    midCamera.x = actualTransform.position.x + maxDistX;
+                }
+                else if (midCamera.x < actualTransform.position.x)
+                {
+                    midCamera.x = actualTransform.position.x - maxDistX;
+                }
+            }
+
+            if (distanceZ > maxDistZ)
+            {
+                if (midCamera.z > actualTransform.position.z)
+                {
+                    midCamera.z = actualTransform.position.z + maxDistZ;
+                }
+                else if (midCamera.z < actualTransform.position.z)
+                {
+                    midCamera.z = actualTransform.position.z - maxDistZ;
+                }
+            }
 
             transform.position += (midCamera - transform.position) * speed * Time.deltaTime;
         }
     }
 
+    //Mira un punto exacto que se le haya pasado
     void SeePoint()
     {
         transform.position += (pointToView - transform.position) * speed * Time.deltaTime;
     }
+    #endregion
 
-
+    //Corrutina para esperar el tiempo de la vision del objeto
     IEnumerator SeeObjectoCorroutine(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
---
+++ Assets/Playgorunds/Maiine/Scrpits/Isonetric/Player/Player.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isActive;
+
+    public GameObject realCharacter;
+    public GameObject totemCharacter;
+
+    IPlayerInteractable _playerInteractable = null;
+
+    MovementController myMovementController;
+    ButtonsController myButtonController;
+    public CameraController cameraController;
+
+
+    private void Start()
+    {
+        myMovementController = new MovementController(transform, speed);
+        myButtonController = new ButtonsController(this, myMovementController, cameraController);
+
+        EventManager.Subscribe("ChangePlayer", ChangeCharacter);
+
+        if (isActive)
+        {
+            realCharacter.SetActive(true);
+            totemCharacter.SetActive(false);
+            myMovementController.ChangeToMove();
+            myButtonController.ButtonsOn();
+        }
+        else
+        {
+            realCharacter.SetActive(false);
+            totemCharacter.SetActive(true);
+            myMovementController.ChangeToStay();
+            myButtonController.ButtonsOff();
+        }
+    }
+
+    void Update()
+    {
+        myButtonController.OnUpdate();
+        myMovementController.actualMovement();
+    }
+
+    public void ChangeCharacter(params object[] parameter)
+    {
+        Debug.Log("aa?");
+        if (isActive)
+        {
+            myMovementController.ChangeToStay();
+            myButtonController.ButtonsOff();
+            realCharacter.SetActive(false);
+            totemCharacter.SetActive(true);
+        }
+        else
+        {
+            myMovementController.ChangeToMove();
+            myButtonController.ButtonsOn();
+            realCharacter.SetActive(true);
+            totemCharacter.SetActive(false);
+        }
+
+        isActive = !isActive;
+    }
+
+    public void CheckInteractable()
+    {
+        if (_playerInteractable == null) return;
+
+        _playerInteractable.DoAction();
+
+        //Mover dentro del objecto
+        //EventManager.Trigger("SeeObject", _playerInteractable.GetPos(), _playerInteractable.GetTime());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IPlayerInteractable actualPlayerInteractable = other.gameObject.GetComponent<IPlayerInteractable>();
+        if (actualPlayerInteractable != null)
+        {
+            _playerInteractable = actualPlayerInteractable;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IPlayerInteractable actualPlayerInteractable = other.gameObject.GetComponent<IPlayerInteractable>();
+        if (actualPlayerInteractable != null)
+            _playerInteractable = null;
+    }
+}
